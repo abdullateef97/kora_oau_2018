fc9a9bf0d42cd3fd6ca124f43f17f9982fad8308
Anifowose <Quadriyanney@gmail.com>
2018-07-28 03:43:12 +0100

    Added appbar to GroupDetails View

---
+++ app/src/main/java/com/thanos/kontribute/ui/group_detail/GroupDetailContract.kt
@@ -0,0 +1,13 @@
+package com.thanos.kontribute.ui.group_detail
+
+import com.thanos.kontribute.ui.base.BasePresenter
+import com.thanos.kontribute.ui.base.BaseView
+
+interface GroupDetailContract {
+
+    interface GroupDetailPresenter: BasePresenter<GroupDetailView> {
+    }
+
+    interface GroupDetailView: BaseView {
+    }
+}

bb500eb9164ce05df0291c5ffc1a097bf1d124c8
abdullateef97 <adeniranyusufabdullateef@gmail.com>
2018-07-28 16:02:47 +0100

    get Aajo Url

--- routes/walletRoute.js
+++ routes/walletRoute.js
@@ -8,6 +8,7 @@ const WalletController = require('../controllers/WalletController');
 
 router.post('/kolo', verifyToken, WalletController.createKolo);
 router.get('/', verifyToken, WalletController.getAllWallets);
-
+router.get('/aajo', verifyToken, WalletController.createAajo);
+router.get('/get_aajo_link', verifyToken, WalletController.getAajoUrl);
 
 module.exports = router
--- services/WalletService.js
+++ services/WalletService.js
@@ -1,7 +1,8 @@
 const Wallet = require('../models/Wallet');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const kolo = 'Kolo';
-
+const aajo = 'Aajo';
     const createWallet = (owner_id) => {
         return new Promise((resolve, reject) => {
             const newWallet = new Wallet({
@@ -40,6 +41,20 @@ const kolo = 'Kolo';
         newKolo.save().then(wallet => resolve(wallet)).catch(err => reject(err));
     })
 
+    createAajo = (aajoObj, user_id) => new Promise((resolve, reject) => {
+        const newAajo = new Wallet({
+            owners: [user_id],
+            creator: user_id,
+            name: aajo,
+            description: aajo.description || 'Aajo',
+            end_date: aajo.end_date,
+            deduction_amount: kolo.deduction_amount,
+            deduction_interval: aajo.deduction_interval
+        })
+
+        newAajo.save().then(wallet => resolve(wallet)).catch(err => reject(err));
+    })
+
     getAllWallets = (owner_id) => new Promise((resolve, reject) => {
         const condition = { $or : [{creator: owner_id}, {owners: owner_id}]};
         Wallet.find(condition).then(wallet => {
@@ -47,6 +62,19 @@ const kolo = 'Kolo';
         }).catch(err => reject(err));
     })
 
+    getAajoUrl = (user_id, wallet_id)=> {
+        const aajoObj = {user_id, wallet_id};
+        const token = generateAajoToken(aajoObj);
+        return token;   
+    }
+
+    generateAajoToken = (userObj) => {
+       const token = jwt.sign(userObj, process.env.JWT_SECRET);
+       return token
+    }
+
+    
 
 
-module.exports = {createWallet, pushWalletToUser, createKolo, getAllWallets};
+module.exports = {createWallet, pushWalletToUser, createKolo, getAllWallets, createAajo,
+                    getAajoUrl,};

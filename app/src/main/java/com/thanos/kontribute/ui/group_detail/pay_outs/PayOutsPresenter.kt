e9563cc4fd2cb3303a4c3598590ae0ebe4dc8e6a
Kehinde Emmanuel <emmakoko1996@gmail.com>
2018-07-28 14:51:26 +0100

    Clean up

--- app/src/main/java/com/thanos/kontribute/ui/group_detail/pay_outs/PayOutsPresenter.kt
+++ app/src/main/java/com/thanos/kontribute/ui/group_detail/pay_outs/PayOutsPresenter.kt
@@ -13,14 +13,14 @@ class PayOutsPresenter : PayOutsContract.PayOutsPresenter {
     override fun fetchGroupPayOuts() {
         payOutsView?.showProgress()
         val payOuts = arrayListOf(
-                Transaction(amount = 90000, group = "Main One", date = "25/04/2018", deposit = true, withdrawal = false),
-                Transaction(amount = 90000, group = "Germany Straight", date = "22/03/2018", deposit = true, withdrawal = false),
-                Transaction(amount = 4500, group = "Main One", date = "28/01/2018", deposit = true, withdrawal = false),
-                Transaction(amount = 30000, group = "Ajo", date = "12/12/2017", deposit = false, withdrawal = true),
-                Transaction(amount = 90000, group = "Main One", date = "25/04/2018", deposit = true, withdrawal = false),
-                Transaction(amount = 90000, group = "Germany Straight", date = "22/03/2018", deposit = true, withdrawal = false),
-                Transaction(amount = 4500, group = "Main One", date = "28/01/2018", deposit = true, withdrawal = false),
-                Transaction(amount = 30000, group = "Ajo", date = "12/12/2017", deposit = false, withdrawal = true)
+                Transaction(amount = 90000, group = "Quadri Anifowose", date = "25/04/2018", deposit = true, withdrawal = false),
+                Transaction(amount = 90000, group = "Lateef Adeniran", date = "22/03/2018", deposit = true, withdrawal = false),
+                Transaction(amount = 4500, group = "Rasheed Sulayman", date = "28/01/2018", deposit = true, withdrawal = false),
+                Transaction(amount = 30000, group = "Emmanuel Kehinde", date = "12/12/2017", deposit = false, withdrawal = true),
+                Transaction(amount = 90000, group = "Emmanuel Kehinde", date = "25/04/2018", deposit = true, withdrawal = false),
+                Transaction(amount = 90000, group = "Rasheed Sulayman", date = "22/03/2018", deposit = true, withdrawal = false),
+                Transaction(amount = 4500, group = "Emmanuel Kehinde", date = "28/01/2018", deposit = true, withdrawal = false),
+                Transaction(amount = 30000, group = "Rasheed Sulayman", date = "12/12/2017", deposit = false, withdrawal = true)
         )
         payOutsView?.hideProgress()
         payOutsView?.showGroupPayouts(payOuts)
